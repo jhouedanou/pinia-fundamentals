022fca66aad1c377c55db6b51bfe2a975b20cb86
Jean Luc Houedanou <jeanluc@bigfiveabidjan.com>
2022-12-15 18:28:34 +0000

    l2

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,8 +1,28 @@
 <script setup>
+  import  {ref} from 'vue'
+  import {useTodoListStore} from "../stores/todoList"
+const todo = ref('')
+const store = useTodoListStore()
+//function which checks if the input is not empty
+function addItemAndClear(item){
+  if(item.length===0){
+    return
+  }
+  //add a new toDo item to the array of todos
+  store.addTodo(item)
+  //clear out the todo ref
+  todo.value = ''
+
+}
+
+
 </script>
 
 <template>
-  <div></div>
+  <form @submit.prevent="addItemAndClear(todo)">
+    <input v-model="todo" type="text" placeholder="task"/>
+    <button>Add</button>
+  </form>
 </template>
 
 <style scoped>
--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -2,6 +2,26 @@ import { defineStore } from 'pinia'
 
 export const useTodoListStore = defineStore('todoList', {
   state: () => ({
-    todoList: []
-  })
+    todoList: [],
+    id:0
+  }),
+  actions:{
+    addTodo(item){
+      //Save to state
+      this.todoList.push({item:item, id:this.id++,completed:false})
+      //add to database 
+   /*    try{
+        const res = await axios.get('localhost:3000/todos');
+        this.todos = res.data;
+      }catch(error){
+        console.log(error);
+      }
+ */
+    },
+    deleteTodo(item){
+      this.todoList = this.todoList.filter((object)=>{
+        return object.id !== item.id
+      })
+    }
+  }
 })
